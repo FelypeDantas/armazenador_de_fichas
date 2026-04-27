4989563d76b4feac08bd2b162bc29c804c53b7d2
Felype <felyped03@gmail.com>
2026-04-27 19:40:22 -0300

    Update ThemeProvider.tsx

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode } from "react";
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
+type ThemeProviderProps = {
+  children: ReactNode;
+};
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
       defaultTheme="dark"
       enableSystem
+      disableTransitionOnChange
     >
       {children}
     </NextThemesProvider>
